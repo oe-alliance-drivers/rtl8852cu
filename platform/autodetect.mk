a5ea3d9f1248e0e556ab1ea92cf48e44ad501af3
morrownr <morrownr@gmail.com>
2026-02-10 18:43:56 -0600

    cleanup autodetect.mk

--- platform/autodetect.mk
+++ platform/autodetect.mk
@@ -1,16 +1,16 @@
 ifeq ($(CONFIG_PLATFORM_AUTODETECT), y)
-ccflags-y += -DCONFIG_LITTLE_ENDIAN
-ccflags-y += -DCONFIG_IOCTL_CFG80211 -DRTW_USE_CFG80211_STA_EVENT
-ccflags-y += -DCONFIG_RADIO_WORK
+EXTRA_CFLAGS += -DCONFIG_LITTLE_ENDIAN
+EXTRA_CFLAGS += -DCONFIG_IOCTL_CFG80211 -DRTW_USE_CFG80211_STA_EVENT
+EXTRA_CFLAGS += -DCONFIG_RADIO_WORK
 
 # Ensure the driver is using the regulatory information from the OS
-ccflags-y += -DCONFIG_REGD_SRC_FROM_OS
+EXTRA_CFLAGS += -DCONFIG_REGD_SRC_FROM_OS
 
 # May be needed to ensure all channels are scanned
-ccflags-y += -DCONFIG_FORCE_SW_CHANNEL_PLAN
+#EXTRA_CFLAGS += -DCONFIG_FORCE_SW_CHANNEL_PLAN
 
 # Enable concurrent mode
-#ccflags-y += -DCONFIG_CONCURRENT_MODE
+#EXTRA_CFLAGS += -DCONFIG_CONCURRENT_MODE
 
 # Ensure correct ARCH is sent to gcc for numerous platforms
 SUBARCH := $(shell uname -m | sed -e "s/i.86/i386/; s/aarch64/arm64/; s/armv.l/arm/; s/riscv.*/riscv/; s/ppc/powerpc/;")
